fc62c8b330decac0be9beae03cc63b4c6ff58822
pasb123 <pasquinel2space@gmail.com>
2022-12-12 14:29:14 +0100

    saved the json data

--- test.py
+++ test.py
@@ -5,6 +5,30 @@ data=json.load(j) #load the data
 app_id=data[inp]['appid'] #save app_id
 name=data[inp]['name'] #save name
 release_date=data[inp]['release_date']# save release date
-print(app_id,name,release_date) #print to test
+english=data[inp]['english'] #save whether the game is in english
+if english == 1: #if its 1 return yes
+    english= True
+else:
+    english= False #else return no
+developer=data[inp]['developer'] #save all data
+publisher=data[inp]['publisher']
+platforms=data[inp]['platforms']
+required_age=data[inp]['required_age']
+categories=data[inp]['categories']
+genres=data[inp]['genres']
+steamspy_tags=data[inp]['steamspy_tags']
+achievements=data[inp]['achievements']
+postive_ratings=data[inp]['positive_ratings']
+negative_ratings=data[inp]['negative_ratings']
+average_playtime=data[inp]['average_playtime']
+median_playtime=data[inp]['median_playtime']
+owners=data[inp]['owners']
+price=data[inp]['price']
+if price==0:    #if price =0 return free
+    price='gratis'
 
-j.close()
+
+print(app_id,name,release_date,english,developer,publisher,platforms,required_age,categories,
+      genres,steamspy_tags,achievements,postive_ratings,negative_ratings,average_playtime,median_playtime,owners,price) #print to test
+
+j.close() #close json
